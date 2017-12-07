a78b52c8f5c8562b44f89b01f98b7c0799e64b8d
Shahid Qureshi <shahidsabir21@hotmail.com>
2017-12-07 00:03:32 +0000

    Added constructor and updated UI after fetching data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 
 class App extends Component {
 
-  fetchData = (name) => {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: ''
+    }
+  }
+
+  fetchData = () => {
     const key = '687ffee217080389caa42809f747df91'
     fetch(
       `http://api.openweathermap.org/data/2.5/forecast?q=London,GB&appid=${key}`)
@@ -11,6 +18,7 @@ class App extends Component {
       if(response.ok) {
         response.json().then(data => {
           console.log('data: ', data)
+          this.setState({data})
         })
       } 
     })
@@ -21,16 +29,21 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Your 3 Hourly Forecast for next 5 days</h1>
-        </header>
-        <div className="App-intro">
-          
+    const { data } = this.state
+    if(data !== '') {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Your 3 Hourly Forecast for next 5 days</h1>
+          </header>
+          <div className="App-intro">
+            <p>{JSON.stringify(data)}</p>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (<div>No Data</div>)
+    }
   }
 }
 
